b2c687d9a249f359fb7a02ce0a17ff0111b7f788
gbelouze <gabriel.belouze@ens.psl.eu>
2021-06-17 12:00:09 +0200

    better bench parameters

--- bench.sh
+++ bench.sh
@@ -1,5 +1,6 @@
 export F=50
 export N=1_000_000
-dune exec -- ./bench/bench.exe --minimal --clear --fanout $F --page 4700 -n $N --with-profiling -v
-python bench/graph.py _bench/replace_random --with-log
-open _bench/replace_random/stats.png
+export S=50_000_000
+if dune exec -- ./bench/bench.exe --minimal --clear --fanout $F --page 5000 -n $N --with-profiling -vv --size $S ; then
+  python3 bench/graph.py _bench/replace_random --with-log
+fi
